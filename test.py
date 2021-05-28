335a575df90d31a874fe5db5772b2dda9fad3a6f
Daisy <ramired2@oregonstate.edu>
2021-05-27 17:50:11 -0700

    fixed linting errors

--- test.py
+++ test.py
@@ -5,19 +5,16 @@ import example
 class TestCase(unittest.TestCase):
 
     def test_add_1(self):
-        self.assertEqual(example.add(1,2), 3)
-
+        self.assertEqual(example.add(1, 2), 3)
 
     def test_sub_1(self):
-        self.assertEqual(example.sub(5,2), 3)
-
+        self.assertEqual(example.sub(5, 2), 3)
 
     def test_mult_1(self):
-        self.assertEqual(example.mult(1,2), 2)
-
+        self.assertEqual(example.mult(1, 2), 2)
 
     def test_div_1(self):
-        self.assertEqual(example.div(6,2), 3)
+        self.assertEqual(example.div(6, 2), 3)
 
 
 if __name__ == '__main__':
